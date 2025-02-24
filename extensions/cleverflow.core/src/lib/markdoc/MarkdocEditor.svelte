b8dd1ba846d48fd49204aaaf7f15489e331f510a
thangnd-uit07 <thangnd.uit07@gmail.com>
2025-02-24 17:30:44 +0700

    MarkdownEditor run in tabs mode

--- extensions/cleverflow.core/src/lib/markdoc/MarkdocEditor.svelte
+++ extensions/cleverflow.core/src/lib/markdoc/MarkdocEditor.svelte
@@ -1,36 +1,30 @@
 <svelte:options customElement="markdoc-editor" />
 
 <script>
-	import { Carta, MarkdownEditor } from 'carta-md';
-	import DOMPurify from 'isomorphic-dompurify';
-	import css from '../../app.css?inline';
-	import cartaCss from 'carta-md/default.css?inline'; /* Default theme */
+    import { Carta, MarkdownEditor } from "carta-md";
+    import DOMPurify from "isomorphic-dompurify";
+    import css from "../../app.css?inline";
+    import cartaCss from "carta-md/default.css?inline"; /* Default theme */
     // SMELL: This is a workaround to make TailwindCSS work in the web component.
     // IMPORTANT: this unuse import is required to make TailwindCSS work in the web component.
-    import Tailwindcss from '../common/components/Tailwindcss.svelte';
+    import Tailwindcss from "../common/components/Tailwindcss.svelte";
 
-	let { 
-        name = 'dummy.mdoc', 
-        text = '',
-     } = $props();
+    let { name = "dummy.mdoc", text = "" } = $props();
 
     let markdownValue = $state(text);
 
-	const carta = new Carta({
-		// Remember to use a sanitizer to prevent XSS attacks!
-		// More on that below
-		sanitizer: DOMPurify.sanitize,
-        mode: "split",
-        placeholder: "Type here...",
-        selectedTab: "write",
-	});
+    const carta = new Carta({
+        // Remember to use a sanitizer to prevent XSS attacks!
+        // More on that below
+        sanitizer: DOMPurify.sanitize,
+    });
 
     export const getMarkdown = () => {
         return markdownValue;
-    }
-	
+    };
 </script>
-<svelte:element this={'style'}>{@html css}</svelte:element>
-<svelte:element this={'style'}>{@html cartaCss}</svelte:element>
 
-<MarkdownEditor {carta} bind:value={markdownValue} />
+<svelte:element this={"style"}>{@html css}</svelte:element>
+<svelte:element this={"style"}>{@html cartaCss}</svelte:element>
+
+<MarkdownEditor {carta} mode={"tabs"} bind:value={markdownValue} />
