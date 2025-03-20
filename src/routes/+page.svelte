7a991b3368e1421395a1ad97eae63e4f6c17cada
thangnd-uit07 <thangnd.uit07@gmail.com>
2025-03-20 22:54:00 +0700

    Rename cleverflow.core to cleverflow.core.frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,9 @@
   import { Tabs } from "@skeletonlabs/skeleton-svelte";
   import { Pencil, Eye, Loader, CircleX, Check, Zap } from "lucide-svelte";
   import { onMount } from "svelte";
-  import MarkdocRendererController from "@cleverflow/cleverflow.core/webcomponents/markdoc-renderer-controller.js";
-  import * as MarkocNodeUtil from "../../extensions/cleverflow.core/src/lib/common/utils/MarkdocNodeUtil.js";
-  import * as JsonUtil from "../../extensions/cleverflow.core/src/lib/common/utils/JsonUtil.js";
+  import MarkdocRendererController from "@cleverflow/cleverflow.core.frontend/webcomponents/markdoc-renderer-controller.js";
+  import * as MarkocNodeUtil from "../../extensions/cleverflow.core.frontend/src/lib/common/utils/MarkdocNodeUtil.js";
+  import * as JsonUtil from "../../extensions/cleverflow.core.frontend/src/lib/common/utils/JsonUtil.js";
 
   const eventServer = "ws://localhost:8080";
   const eventServerToken = "76de3ba222bec3af21f9dbfb01f3197b";
@@ -20,9 +20,11 @@
   let markdoc = $state(``);
 
   onMount(async () => {
-    await import("@cleverflow/cleverflow.core/webcomponents/markdoc-editor.js");
     await import(
-      "@cleverflow/cleverflow.core/webcomponents/markdoc-renderer.js"
+      "@cleverflow/cleverflow.core.frontend/webcomponents/markdoc-editor.js"
+    );
+    await import(
+      "@cleverflow/cleverflow.core.frontend/webcomponents/markdoc-renderer.js"
     );
 
     markdocRendererController = new MarkdocRendererController(
