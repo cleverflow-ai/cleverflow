e5c6ddc789b6e245f1cf75f1afd6e33adc558926
thangnd-uit07 <thangnd.uit07@gmail.com>
2025-05-20 23:48:58 +0700

    Merge branch 'feature/initialization' of github.com:cleverflow-ai/cleverflow into feature/initialization # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- extensions/cleverflow.core.frontend/src/lib/dynamicform/JsonForm.svelte
+++ extensions/cleverflow.core.frontend/src/lib/dynamicform/JsonForm.svelte
@@ -4,6 +4,8 @@
 	import { onMount, onDestroy } from "svelte";
 	import css from "../../app.css?inline";
 	import postal from "postal";
+	import "@jsfe/shoelace";
+	import type { FromSchema, JSONSchema7, Jsf } from "@jsfe/shoelace";
 
 	let props = $props();
 	let { url = "", text = "", id = null, theme = "crimson" } = props;
@@ -64,6 +66,85 @@
 			});
 		}
 	};
+
+	const mySchema = {
+		title: "Sign-up form",
+		description: "A simple form example.",
+		type: "object",
+		required: ["FirstName", "LastName"],
+		properties: {
+			FirstName: {
+				type: "string",
+				title: "First name",
+				default: "Chuck",
+			},
+			LastName: {
+				type: "string",
+				title: "Last name",
+			},
+			Age: {
+				type: "integer",
+				title: "Age",
+				minimum: 13,
+				maximum: 150,
+			},
+			Bio: {
+				type: "string",
+				title: "Bio",
+			},
+			Email: {
+				title: "Email",
+				format: "email",
+				type: "string",
+			},
+			Password: {
+				type: "string",
+				title: "Password",
+				format: "password",
+				minLength: 3,
+			},
+			Telephone: {
+				type: "string",
+				title: "Telephone",
+				minLength: 10,
+			},
+		},
+	} as const satisfies JSONSchema7;
+	type MyData = FromSchema<typeof mySchema>;
+
+	function assertValidData(data: unknown): data is MyData {
+		// Use your AJV or other schema checker here, if you need thorough validation
+		// ...
+		return true;
+	}
+
+	let dataInSvelte: MyData = $state({
+		foo: "hello",
+	});
+
+	function formBinding(form: Jsf) {
+		form.data = dataInSvelte;
+		form.schema = /* Type-casted as JSONSchema7 */ mySchema;
+		form.uiSchema = {
+			/* Type-casted as UiSchema */
+			bar: {
+				"ui:widget": "switch",
+			},
+		};
+		form.dataChangeCallback = (newData: any) => {
+			console.log({ "Data from Svelte": newData });
+
+			if (assertValidData(newData)) dataInSvelte = newData;
+			else console.error("Invalid data!");
+		};
+		form.submitCallback = (newData: any, valid: any) => {
+			console.log({ "Submitted from Svelte!": newData, valid });
+
+			if (assertValidData(newData)) {
+				// Do stuff...
+			}
+		};
+	}
 </script>
 
 <svelte:element this={"style"}>{@html css}</svelte:element>
@@ -84,6 +165,8 @@
 {/snippet} -->
 
 <main data-theme={theme}>
-	<div>this is JSON SCHEMA FORM</div>
+	<article id="svelte">
+		<jsf-shoelace use:formBinding></jsf-shoelace>
+	</article>
 	<div bind:this={serverWebComponentContainer}></div>
 </main>
