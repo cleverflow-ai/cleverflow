87cbf4f43ec1e6066a374981853096bf176de50a
thangnd-uit07 <thangnd.uit07@gmail.com>
2025-05-26 19:41:34 +0700

    Display glb from mcp 3d

--- src/routes/test-mcp-io/+page.svelte
+++ src/routes/test-mcp-io/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { createClient } from "@cleverflow-ai/cleverflow.mcp.io/dist/client.js";
     import MarkdocRenderer from "$lib/components/MarkdocRenderer.svelte";
+    import GlbViewer from "$lib/components/GlbViewer.svelte";
 
     let baseUrl = $state("https://docs-atlascopco.clevernow.com/api");
     let apiKey = $state("ol_api_qHHZcDQ8EKJfx9tKwbi7E2zeT30Rnx5bq1uMfO");
@@ -14,7 +15,13 @@
     let repo = $state("atlascopco-dasm");
     let filePath = $state("README.md");
 
-    let currentForm = $state("gitea-form");
+    // RP
+    let rpUsername = $state("");
+    let rpPassword = $state("");
+    let rpModelName = $state("");
+
+    let currentForm = $state("glb-form");
+    // let currentForm = $state("gitea-form");
     // let currentForm = $state("gitea-form");
 
     let results: any[] = $state([]);
@@ -41,20 +48,7 @@
         // }` //await guiAgent.runTask(`generate-json-form`, `Please help me generate a .. with three text fields for ...`);
     });
 
-    function setDefaultForm(): void {
-        if (currentForm === "outline-form") {
-            apiKey = "ol_api_qHHZcDQ8EKJfx9tKwbi7E2zeT30Rnx5bq1uMfO"; // Clear apiKey for Outline form
-            baseUrl = "https://docs-atlascopco.clevernow.com/api"; // Clear baseUrl for Outline form
-            fileId = "sealing-technologies-rrOT5m2iSz"; // Clear fileId for Outline form
-        } else if (currentForm === "gitea-form") {
-            apiKey = "04f4b0dada8fa8632dc7541f2f2131c703693e7e"; // Clear apiKey for Gitea form
-            baseUrl =
-                "https://gitea-atlascopco-integration.clevernow.com/api/v1"; // Clear baseUrl for Gitea form
-            repoOwner = "clevernow"; // Clear repoOwner for Gitea form
-            repo = "atlascopco-dasm"; // Clear repo for Gitea form
-            filePath = "README.md"; // Clear filePath for Gitea form
-        }
-    }
+    function setDefaultForm(): void {}
 
     async function fetchOutlineTextFile(): Promise<void> {
         if (!baseUrl || !apiKey || !fileId) {
@@ -83,17 +77,20 @@
         // const tools = await client.listTools();
         // console.log("Tools:", tools);
 
-        const result = await client.callTool({
-            name: "fetch-outline-text-file",
-            arguments: {
-                fileId,
-                baseUrl,
-                apiKey,
-                // fileId: "sealing-technologies-rrOT5m2iSz",
-                // baseUrl: "https://docs-atlascopco.clevernow.com/api",
-                // apiKey: "ol_api_qHHZcDQ8EKJfx9tKwbi7E2zeT30Rnx5bq1uMfO",
+        const result = await client.callTool(
+            {
+                name: "fetch-outline-text-file",
+                arguments: {
+                    fileId,
+                    baseUrl,
+                    apiKey,
+                },
             },
-        });
+            null,
+            {
+                timeout: 3600 * 1000,
+            },
+        );
         console.log("Result:", result);
         const text =
             result.content &&
@@ -113,9 +110,6 @@
             ];
         }
         console.log(results);
-        // fileId = ""; // Clear fileId after fetching
-        // baseUrl = ""; // Clear baseUrl after fetching
-        // apiKey = ""; //
     }
 
     async function fetchGiteaTextFile(): Promise<void> {
@@ -126,16 +120,22 @@
 
         const client = await createClient("http://localhost:3000/mcp");
 
-        const result = await client.callTool({
-            name: "fetch-gitea-text-file",
-            arguments: {
-                baseUrl,
-                apiKey,
-                repoOwner,
-                repo,
-                filePath,
+        const result = await client.callTool(
+            {
+                name: "fetch-gitea-text-file",
+                arguments: {
+                    baseUrl,
+                    apiKey,
+                    repoOwner,
+                    repo,
+                    filePath,
+                },
             },
-        });
+            // null,
+            // {
+            //     timeout: 3600 * 1000,
+            // },
+        );
         const text =
             result.content &&
             Array.isArray(result.content) &&
@@ -156,6 +156,51 @@
             ]; // Ensure results is an array
         }
     }
+
+    async function convertStepToGlb(): Promise<void> {
+        if (!repoOwner || !repo || !filePath) {
+            console.error("Please provide repoOwner, repo, and filePath.");
+            return;
+        }
+
+        const client = await createClient("http://localhost:4001/mcp");
+
+        const result = await client.callTool(
+            {
+                name: "convert-step-to-glb",
+                arguments: {
+                    rpUsername,
+                    rpPassword,
+                    rpModelName,
+                },
+            },
+            // null,
+            // {
+            //     timeout: 3600 * 1000,
+            // },
+        );
+        console.log("Result:", result);
+        const data =
+            result.content &&
+            Array.isArray(result.content) &&
+            result.content.length > 0
+                ? result.content[0].data
+                : null;
+        if (data) {
+            console.log("GLB Data:", data.length);
+            results = [
+                {
+                    baseUrl,
+                    apiKey,
+                    repoOwner,
+                    repo,
+                    filePath,
+                    gblData: data,
+                },
+                ...results,
+            ]; // Ensure results is an array
+        }
+    }
 </script>
 
 <div class="max-w-2xl mx-auto p-4 space-y-6">
@@ -167,6 +212,7 @@
                 onchange={setDefaultForm}
                 class="select select-bordered w-full max-w-xs"
             >
+                <option value="glb-form">convert-step-to-glb</option>
                 <option value="outline-form">Outline Text File</option>
                 <option value="gitea-form">Gitea Text File</option>
             </select>
@@ -220,6 +266,30 @@
                     class="btn btn-sm preset-filled-primary-500">Submit</button
                 >
             </div>
+        {:else if currentForm === "glb-form"}
+            <label class="label">
+                <span class="label-text">RP Username</span>
+                <input bind:value={rpUsername} type="text" class="input" />
+            </label>
+
+            <label class="label">
+                <span class="label-text">RP Password</span>
+                <input bind:value={rpPassword} type="password" class="input" />
+            </label>
+
+            <label class="label">
+                <span class="label-text">RP Model Name</span>
+                <input bind:value={rpModelName} type="text" class="input" />
+            </label>
+
+            <div class="flex justify-end">
+                <button
+                    onclick={convertStepToGlb}
+                    disabled={!repoOwner || !repo || !filePath ? true : false}
+                    type="button"
+                    class="btn btn-sm preset-filled-primary-500">Submit</button
+                >
+            </div>
         {/if}
     </form>
 
@@ -227,7 +297,11 @@
     <div class="mx-auto w-full space-y-4">
         {#key results.length}
             {#each results as result}
-                <MarkdocRenderer doc={result.text}></MarkdocRenderer>
+                {#if result.gblData}
+                    <GlbViewer base64Data={result.gblData}></GlbViewer>
+                {:else if result.text}
+                    <MarkdocRenderer doc={result.text}></MarkdocRenderer>
+                {/if}
             {/each}
         {/key}
 
