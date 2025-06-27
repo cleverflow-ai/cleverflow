b07cd7ea5258ffaf8123b35c0d0c04c8935235fc
thangnd-uit07 <thangnd.uit07@gmail.com>
2025-06-28 06:57:06 +0700

    Moved web-components into src folder

---
+++ mcp/cleverflow.mcp.io/src/web-components/src/lib/pdf-viewer/PdfViewer.svelte
@@ -0,0 +1,32 @@
+<svelte:options customElement="pdf-viewer" />
+
+<script>
+	import PdfViewer from "svelte-pdf";
+
+	let { pdfUrl, base64Content, pageNum = 1 } = $props();
+</script>
+
+<div class="relative flex justify-center">
+	<div class="pdfViewer flex justify-center">
+		{#if pdfUrl}
+			<PdfViewer
+				url={pdfUrl}
+				showButtons={["navigation", "pageInfo"]}
+				showBorder={true}
+				pageNum={1}
+				scale={1}
+				showTopButton={false}
+			/>
+		{:else if base64Content}
+			<PdfViewer
+				url={pdfUrl}
+				data={base64Content}
+				showButtons={["navigation", "pageInfo"]}
+				showBorder={true}
+				{pageNum}
+				scale={1}
+				showTopButton={false}
+			/>
+		{/if}
+	</div>
+</div>
